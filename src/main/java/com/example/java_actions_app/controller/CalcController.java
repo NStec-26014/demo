26e6d111faf56457ac39497350f31a6f9e49d538
NStec-26014 <nstec26_014@fullness.email>
2026-07-30 16:10:16 +0900

    コードフォーマットの検証を追加

---
+++ src/main/java/com/example/java_actions_app/controller/CalcController.java
@@ -0,0 +1,13 @@
+package com.example.java_actions_app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class CalcController {
+
+    // わざとインデントを崩す例
+    @GetMapping("/calc")
+public String showForm() {
+            return "calc"; 
+    }
+
+}
